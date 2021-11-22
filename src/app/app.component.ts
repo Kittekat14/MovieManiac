b38712827de92edda70c86806106b81c8fce5fc6
Kittekat14 <katihofstetter@gmail.com>
2021-11-22 12:11:22 +0100

    user registration form implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
 
 @Component({
   selector: 'app-root', // where to put app.component.ts => <app-root> in index.html
@@ -7,4 +10,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'MovieManiac';
+
+  constructor(public dialog: MatDialog) { }
+
+  // this is the function that will open the dialog after clickling signup button
+  openUserRegistrationDialog(): void {
+    this.dialog.open(UserRegistrationFormComponent, {
+      width: '480px'
+    });
+  }
+
+  valueForm = new FormGroup({
+    input: new FormControl(),
+    username: new FormControl()
+  });
+
+  // onSubmit() {
+  //   console.log(this.valueForm.value);
+  // }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+
+
+//import {MatDialogRef} from '@angular/material/dialog'
+// ...
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserRegistrationFormComponent
   ],
   imports: [
     BrowserModule, // responsible for the DOM and needed for Web Applications (not Mobile/Desktop)
     HttpClientModule, // responsible for http requests
-    AppRoutingModule // responsible for Routing
+    AppRoutingModule, // responsible for Routing
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatDialogModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatSnackBarModule
   ],
   providers: [], // to declare another provider other than root-app
   bootstrap: [AppComponent]
