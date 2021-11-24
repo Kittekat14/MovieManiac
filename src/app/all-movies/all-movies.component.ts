9b683c892fc513e2d499b3c108ae3d80461223a8
Kittekat14 <katihofstetter@gmail.com>
2021-11-24 12:25:27 +0100

    getMovies() in allmovies-component right

--- src/app/all-movies/all-movies.component.ts
+++ src/app/all-movies/all-movies.component.ts
@@ -4,23 +4,21 @@ import { FetchApiDataService } from '../fetch-api-data.service';
 @Component({
   selector: 'app-all-movies',
   templateUrl: './all-movies.component.html',
-  styleUrls: ['./all-movies.component.scss']
+  styleUrls: ['./all-movies.component.scss'],
 })
 export class AllMoviesComponent implements OnInit {
   movies: any[] = [];
-    constructor(public fetchApiData: FetchApiDataService) { }
+  constructor(public fetchApiData: FetchApiDataService) {}
 
-    ngOnInit() {
-      this.getMovies();
-    }
+  ngOnInit(): void {
+    this.getMovies();
+  }
 
-  getMovies() {
+  getMovies(): void {
     this.fetchApiData.getAllMovies().subscribe((resp: any) => {
       this.movies = resp;
       console.log(this.movies);
       return this.movies;
     });
   }
-
-
 }
